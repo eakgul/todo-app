64a7f55d8c9d149022bf596071bdb586c884344b
eakgul <esataakgul@gmail.com>
2023-07-30 21:01:42 +0300

    TODO App

--- app.js
+++ app.js
@@ -1,54 +1,101 @@
 
-    const addToDoButton = document.getElementById('addToDo');
-    const toDoContainer = document.getElementById('toDoContainer');
-    const inputText = document.getElementById('inputText');
-    const inputDate = document.getElementById('inputDate');
-    const clearToDo = document.getElementById('clearToDo');
-  
-    addToDoButton.addEventListener('click', function () {
-        if (inputText.value.trim() === '' || inputDate.value.trim() === '') {
-            alert('Lütfen görev ve tarih alanlarını doldurun.'); //Herhangi bir form öğesi boş kalırsa uyarı verecek, işlemi yapmayacak.
-        } else { //Her iki form öğesi de doluysa ana işlemi yapacak.
-            const p = document.createElement('p');
-            p.classList.add('p-styling');
+const addToDoButton = document.querySelector("#addToDo");
+const todoTable = document.querySelector("#todoTable");
+const todoInput = document.querySelector("#inputText");
+const clearToDo = document.querySelector("#clearToDo");
+const notify = document.querySelector(".notify");
+const notifySuccess = document.querySelector(".notifySuccess");
 
-            const checkbox = document.createElement('input');
-            checkbox.setAttribute('type', 'checkbox');
-            p.appendChild(checkbox);
+let todos = [];
 
-            toDoContainer.appendChild(p);
-            p.innerHTML += '- ' + inputText.value + ' - Bitiş Tarihi: ' + inputDate.value;
-            inputText.value = '';
-            inputDate.value = '';
+// Tamamlanan göreve çizgi eklemeyi-silmeyi sağlayan fonksiyon
+const toggleTodoLineThrough = (todoCell) => {
+  todoCell.classList.toggle('line-through');
+  updateLocalStorage();
+};
 
-            p.addEventListener('click', function (event) {
-                if (event.ctrlKey) {
-                    p.style.color = '#FFDBAA'; //Ctrl+tek tık yapıldığında görev sarı oluyor. 
-                } else {
-                    p.style.color = '#7A9D54'; //Tek tık yapıldığında işlem tamamlandı anlamında görev yeşil oluyor.
-                }
-            });
+// Yeni görev ekle
+const addTodo = () => {
+  const trimValue = todoInput.value.trim();
+  const trimDateValue = inputDate.value.trim();
 
-            p.addEventListener('dblclick', function () {
-                p.style.color = '#F31559'; //Çift tık yapıldığında işlem tamamlanmadı anlamında görev kırmızı oluyor.
-            });
+  // Eğer görev veya bitiş tarihi boşsa bildirim göster ve işlem yapma
+  if (trimValue === "" || trimDateValue === "") {
+    showNotify(notify);
+  } else {
+    // Yeni görev eklendiğinde başarılı bildirimi göster
+    showNotify(notifySuccess);
 
-            // Ekleme işleminden sonra güncel liste localstorage a kaydediliyor.
-            localStorage.setItem('todos', toDoContainer.innerHTML);
-        }
+    // Yeni görev için bir tablo oluştur
+    const row = document.createElement("tr");
+    const todoCell = document.createElement("td");
+    todoCell.className = "p-styling";
+    todoCell.innerText = "- " + trimValue + " - Bitiş Tarihi: " + trimDateValue;
+    todoInput.value = "";
+    inputDate.value = "";
+    row.appendChild(todoCell);
+
+    todoTable.querySelector("tbody").appendChild(row);
+
+    todos.push(trimValue);
+    window.localStorage.setItem("todos", JSON.stringify(todos));
+
+    todoCell.addEventListener('click', () => {
+      toggleTodoLineThrough(todoCell);
     });
 
-    clearToDo.addEventListener('click', function () {
-        const p = toDoContainer.querySelectorAll('p');
-
-        p.forEach((p) => {
-            const checkbox = p.querySelector('input[type="checkbox"]');
-            if (checkbox.checked) {
-                toDoContainer.removeChild(p); //Seçili checkboxun olduğu görev siliniyor.
-            }
-        });
-
-        // Silme işleminden sonra güncel liste localstorage a kaydediliyor.
-        localStorage.setItem('todos', toDoContainer.innerHTML);
+    todoCell.addEventListener('dblclick', () => {
+      // Çift tıklanması durumunda görevi sil
+      todoTable.querySelector("tbody").removeChild(row);
+      updateLocalStorage();
     });
+  }
+};
 
+// Local storage'daki görevleri güncelle
+const updateLocalStorage = () => {
+  todos = Array.from(todoTable.querySelectorAll("td.p-styling")).map(td => td.innerText.split(" - ")[1]);
+  window.localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+// Görevleri tablodan ve local storage'dan temizle
+const clearTodo = () => {
+  todoTable.querySelector("tbody").innerHTML = "";
+  todos = [];
+  window.localStorage.removeItem("todos");
+};
+
+// Local storage'dan alınan görevleri tabloya ekle
+const getTodoListFromLS = () => {
+  if (window.localStorage.getItem("todos") != null) {
+    todos = JSON.parse(window.localStorage.getItem("todos"));
+
+    todos.forEach((element) => {
+      const row = document.createElement("tr");
+      const todoCell = document.createElement("td");
+
+      todoCell.className = "p-styling";
+      todoCell.innerText = "- " + element + " - Bitiş Tarihi: " + "x";
+      row.appendChild(todoCell);
+
+      todoTable.querySelector("tbody").appendChild(row);
+    });
+  }
+};
+
+// Bildirim fonksiyonu
+const showNotify = (notifyElement) => {
+  notifyElement.style.display = "block";
+  setTimeout(() => {
+    notifyElement.style.display = "none";
+  }, 3000);
+};
+
+// Sayfa yüklendiğinde çalışacak fonksiyonlar
+const mountEventlistener = () => {
+  document.addEventListener("DOMContentLoaded", getTodoListFromLS);
+  addToDoButton.addEventListener("click", addTodo);
+  clearToDo.addEventListener("click", clearTodo);
+};
+
+mountEventlistener();
